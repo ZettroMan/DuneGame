b6dbed63a3d5ca794606485001bbbece7712e11a
Roman Russkikh <r-ru@yandex.ru>
2020-05-26 02:10:26 +0300

    Lesson3

--- core/src/com/dune/game/core/GameController.java
+++ core/src/com/dune/game/core/GameController.java
@@ -2,6 +2,8 @@ package com.dune.game.core;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameController {
     private BattleMap map;
@@ -25,15 +27,31 @@ public class GameController {
         Assets.getInstance().loadAssets();
         this.map = new BattleMap();
         this.projectilesController = new ProjectilesController(this);
-        this.tank = new Tank(this, 200, 200);;
+        this.tank = new Tank(this, 200, 200);
     }
 
     public void update(float dt) {
+        map.update(dt);
         tank.update(dt);
         projectilesController.update(dt);
         checkCollisions(dt);
     }
 
     public void checkCollisions(float dt) {
+        int tankTileRow = MathUtils.floor(tank.getPosition().y / 80);
+        int tankTileCol = MathUtils.floor(tank.getPosition().x / 80);
+        for (int row = tankTileRow - 1; row <= tankTileRow + 1; row++) {
+            if (row < 0 || row > 8) continue;
+            for (int col = tankTileCol - 1; col <= tankTileCol + 1; col++) {
+                if (col < 0 || col > 15) continue;
+                float tileCenterY = row * 80 + 40;
+                float tileCenterX = col * 80 + 40;
+                if (tank.getPosition().dst(tileCenterX, tileCenterY) < 50) {
+                    map.getGizmos()[row][col] = 0;
+                }
+
+            }
+        }
+
     }
 }
